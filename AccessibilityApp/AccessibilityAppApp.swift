251ba06bd7d28f0ae541e9ce37efc51b457fe04c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-05 23:21:15 -0300

    Initial Commit

---
+++ AccessibilityApp/AccessibilityAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityAppApp.swift
+//  AccessibilityApp
+//
+//  Created by Matheus Viana on 05/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
