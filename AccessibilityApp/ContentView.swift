d552344296800f73053b47adb4ff0012e7819975
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-08 22:44:23 -0300

    rollback

--- AccessibilityApp/ContentView.swift
+++ AccessibilityApp/ContentView.swift
@@ -8,32 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflower",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
-    
     var body: some View {
-        Image(pictures[selectedPicture])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture {
-                selectedPicture = Int.random(in: 0...3)
-            }
-            .accessibilityLabel(labels[selectedPicture])
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
     }
 }
 
