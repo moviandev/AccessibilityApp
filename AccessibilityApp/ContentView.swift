68bc88db9a3f4daf67884edf4f8c6f37af56b9d2
Matheus Viana <matheus_o_viana@hotmail.com>
2023-05-08 22:59:31 -0300

    add adjustable actions to voice over and a label to read the state value

--- AccessibilityApp/ContentView.swift
+++ AccessibilityApp/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var value = 10
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Value: \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .padding()
+        .accessibilityElement()
+        .accessibilityLabel("Value")
+        .accessibilityValue(String(value))
+        .accessibilityAdjustableAction { direction in
+            switch direction {
+            case .decrement:
+                value -= 1
+            case .increment:
+                value += 1
+            default:
+                print("Not handled")
+            }
+        }
     }
 }
 
